42b85fc3329dfd18eec7f2094767be408899d944
Matt Fleming <matthewfleming8@gmail.com>
2020-03-27 20:40:37 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -38,8 +38,8 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  pickle = ["wow", "I", "am", "really", "learning", "arrays!"]
-  print pickle.index[2]
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  array.index[2]
   
 end
 
