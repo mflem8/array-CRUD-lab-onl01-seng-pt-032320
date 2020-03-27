c30817beb1e3d3062391c4ebc479d51c7eb5645a
Matt Fleming <matthewfleming8@gmail.com>
2020-03-27 20:42:06 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -33,7 +33,7 @@ end
 def retrieve_element_from_index(array, index_number)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
   index_number[2]
-  return index_number
+  array[2]
   
 end
 
@@ -45,6 +45,6 @@ end
 
 def retrieve_last_element_from_array(array)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  array.index[-1]
+  array[-1]
   
 end
