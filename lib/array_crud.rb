371c880dc3de5993294059bb2e3fba6ea078e04f
Matt Fleming <matthewfleming8@gmail.com>
2020-03-27 20:13:35 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,7 @@ end
 def add_element_to_end_of_array(array, element)
   array = ["add", "item", "to"]
   element = "end"
-  array.push(element)
+  array.push("end")
 end
 
 def add_element_to_start_of_array(array, element)
